7a225d39b586eedd706d468bff567e3578c866ad
Crisxz1891 <crisxz1891@gmail.com>
2017-06-15 20:09:27 -0300

    Create equipo.h

--- TP_GDT/equipo.h
+++ TP_GDT/equipo.h
@@ -20,25 +20,85 @@ struct Equipo{
 */
 void crear(Equipo &equipo);
 
+/**
+PRE:
+    La instancia del TDA debe haberse creado con anterioridad (primitiva crear) y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    devuelve el id del equipo.
+*/
 int getId(Equipo &equipo);
 
+/**
+PRE:
+    La instancia del TDA debe haberse creado con anterioridad (primitiva crear) y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    devuelve el nombre de la direccion de memoria.
+*/
 char* getNombre(Equipo &equipo);
 
+/**
+PRE:
+    La instancia del TDA debe haberse creado con anterioridad (primitiva crear) y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    devuelve el nombre de usuario de la direccion de memoria.
+*/
 char* getNombreUsuario(Equipo &equipo);
 
 ListaJugador* getJugadores(Equipo &equipo);
 
+/**
+PRE:
+    La instancia del TDA debe haberse creado con anterioridad (primitiva crear) y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    devuelve el puntaje por fecha del equipo.
+*/
 int getPuntajeFecha(Equipo &equipo);
 
+/**
+PRE:
+    La instancia del TDA debe haberse creado con anterioridad (primitiva crear) y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    devuelve el puntaje total del equipo.
+*/
 int getPuntajeTotal(Equipo &equipo);
 
+/**
+PRE:
+    La instancia del TDA debe haberse creado con anterioridad (primitiva crear) y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    Asigna el valor de id a la instancia.
+*/
 void setId(Equipo &equipo,int id);
 
+/**
+PRE:
+    La instancia del TDA debe haberse creado con anterioridad (primitiva crear) y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    Asigna el valor de nombre  a la instancia.
+*/
 void setNombre (Equipo &equipo, char*nombre);
 
+/**
+PRE:
+    La instancia del TDA debe haberse creado con anterioridad (primitiva crear) y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    Asigna el valor de nombre a la instancia.
+*/
 void setNombreUsuario (Equipo &equipo, char*nombre);
 
+/**
+PRE:
+    La instancia del TDA debe haberse creado con anterioridad (primitiva crear) y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    Asigna el valor de puntaje a la instancia.
+*/
 void setPuntajeFecha(Equipo &equipo,int puntaje);
 
+/**
+PRE:
+    La instancia del TDA debe haberse creado con anterioridad (primitiva crear) y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    Asigna el valor de puntaje a la instancia.
+*/
 void setPuntajeTotal(Equipo &equipo, int puntaje);
 #endif // __EQUIPOS_H__
